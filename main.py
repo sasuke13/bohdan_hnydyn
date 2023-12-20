991f2de89d9710cc0d054946c49f935bd3fd7b5a
sasuke13 <bgnydyn@gmail.com>
2023-12-20 19:41:14 +0200

    changed call of print_hi again

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Bohdan')
+    print_hi('Bohdan and world!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
